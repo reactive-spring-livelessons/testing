a5179f7738793e3742848b2eb31e2f4d6f97f3ef
Josh Long <starbuxman@gmail.com>
2018-04-05 00:09:34 -0700

    fluxflixservice

--- movie-service/src/test/java/com/example/movieservice/FluxFlixServiceTest.java
+++ movie-service/src/test/java/com/example/movieservice/FluxFlixServiceTest.java
@@ -1,11 +1,14 @@
 package com.example.movieservice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
@@ -14,19 +17,41 @@ import java.time.Duration;
 @SpringBootTest
 public class FluxFlixServiceTest {
 
-    @Autowired
-    private FluxFlixService fluxFlixService;
+		private WebTestClient webTestClient;
 
-    @Test
-    public void eventsTake10() throws Exception {
-        Movie movie = this.fluxFlixService.all().blockFirst();
+		@Autowired
+		private FluxFlixService fluxFlixService;
 
-        StepVerifier.withVirtualTime(
-                () -> this.fluxFlixService.events(movie.getId())
-                        .take(10)
-                        .collectList())
-                .thenAwait(Duration.ofHours(1))
-                .consumeNextWith(list -> Assert.assertEquals(10, list.size()))
-                .verifyComplete();
-    }
+		@Autowired
+		private ApplicationContext applicationContext;
+
+		@Before
+		public void before() throws Exception {
+				this.webTestClient = WebTestClient
+					.bindToApplicationContext(this.applicationContext)
+					.configureClient()
+					.baseUrl("http://localhost:8080/")
+					.build();
+		}
+
+		@Test
+		public void getMoviesReturnsOk() {
+				this.webTestClient
+					.get()
+					.uri("/movies")
+					.exchange()
+					.expectStatus()
+					.isOk();
+		}
+
+		@Test
+		public void eventsTake10() {
+				Movie movie = this.fluxFlixService.all().blockFirst();
+				StepVerifier.withVirtualTime(() -> this.fluxFlixService.events(movie.getId())
+					.take(10)
+					.collectList())
+					.thenAwait(Duration.ofHours(1))
+					.consumeNextWith(list -> Assert.assertEquals(10, list.size()))
+					.verifyComplete();
+		}
 }
